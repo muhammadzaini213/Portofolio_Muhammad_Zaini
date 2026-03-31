c6aba770d9ef752dce54b3e230a8df3d6a880a71
muhammadzaini213 <oniichanterritory@gmail.com>
2026-04-01 02:38:55 +0800

    add: sitemap generator beta

--- scripts/generate-sitemap.cjs
+++ scripts/generate-sitemap.cjs
@@ -52,8 +52,6 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <url><loc>${BASE_URL}/</loc><priority>1.0</priority></url>
   <url><loc>${BASE_URL}/articles</loc><priority>0.8</priority></url>
   ${articles.map(a => `<url><loc>${BASE_URL}/articles/${a.slug}</loc><priority>0.7</priority></url>`).join('')}
-  <url><loc>${BASE_URL}/projects</loc><priority>0.8</priority></url>
-  <url><loc>${BASE_URL}/#contact</loc><priority>0.6</priority></url>
 </urlset>`;
 
 fs.writeFileSync(path.join(__dirname, '../public/sitemap.xml'), sitemap);
