f41a44d3b79fbb1ea2722fb11a09783409335190
muhammadzaini213 <oniichanterritory@gmail.com>
2026-05-15 01:04:59 +0800

    middleware on root updated with config

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,8 @@
-export { middleware, config } from "@/utils/supabase/middleware";
+import { middleware } from "@/utils/supabase/middleware";
+export default middleware;
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
