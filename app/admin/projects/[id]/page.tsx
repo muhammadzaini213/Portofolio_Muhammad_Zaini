8992451d90548be718c24582fd76df082c9f8de4
muhammadzaini213 <oniichanterritory@gmail.com>
2026-05-15 01:15:02 +0800

    add generate static params in page projects admin edit

--- app/admin/projects/[id]/page.tsx
+++ app/admin/projects/[id]/page.tsx
@@ -1,10 +1,14 @@
-export const dynamic = 'force-dynamic' // Taruh di baris paling atas
+export const dynamic = 'force-dynamic'
+export const dynamicParams = true
+
+export async function generateStaticParams() {
+  return []
+}
 
 import { prisma } from "@/lib/prisma"
 import { notFound } from "next/navigation"
 import EditProjectClient from "./EditProjectClient"
 
-// Next.js 15: params adalah Promise
 export default async function EditProjectPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
   const project = await prisma.project.findUnique({ where: { id } })
