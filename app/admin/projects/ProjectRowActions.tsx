a07b3754f911c743f35d97a78dad23b41dcd318b
muhammadzaini213 <oniichanterritory@gmail.com>
2026-05-23 01:43:29 +0800

    fix:projects edit page auto redirect - 3

--- app/admin/projects/ProjectRowActions.tsx
+++ app/admin/projects/ProjectRowActions.tsx
@@ -3,7 +3,7 @@
 
 import { useState } from "react"
 import { Loader2, Trash2 } from "lucide-react"
-import { deleteProject } from "./[id]/actions"
+import { deleteProject } from "./[slug]/actions"
 import { useRouter } from "next/navigation"
 
 export default function ProjectRowActions({ id }: { id: string }) {
